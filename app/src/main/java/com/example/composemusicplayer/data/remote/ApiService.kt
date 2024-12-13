89c2a758a294bb7a702fd9848e3720d3cdba035f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-13 22:21:41 +0530

    Add API service interface for fetching music data

--- app/src/main/java/com/example/composemusicplayer/data/remote/ApiService.kt
+++ app/src/main/java/com/example/composemusicplayer/data/remote/ApiService.kt
@@ -1,4 +1,11 @@
 package com.example.composemusicplayer.data.remote
 
+import com.example.composemusicplayer.data.model.MusicModelItem
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET("JSON/music.json")
+    fun getMusic():Call<List<MusicModelItem>>
 }
--- app/src/main/java/com/example/composemusicplayer/data/remote/ApiURL.kt
+++ app/src/main/java/com/example/composemusicplayer/data/remote/ApiURL.kt
@@ -1,4 +1,5 @@
 package com.example.composemusicplayer.data.remote
 
 object ApiURL {
+    const val API_URL = "https://viditsavaliya19.github.io/"
 }
