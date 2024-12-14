413b3cdbd7a7fd944b2da3d67c341fdb0b95186f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-14 18:44:22 +0530

    Add MusicControllerUiState data class to manage music player UI state

--- app/src/main/java/com/example/composemusicplayer/data/model/MusicControllerUiState.kt
+++ app/src/main/java/com/example/composemusicplayer/data/model/MusicControllerUiState.kt
@@ -1,3 +1,13 @@
 package com.example.composemusicplayer.data.model
 
-data class MusicControllerUiState()
+import com.example.composemusicplayer.domain.model.Song
+import com.example.composemusicplayer.domain.other.PlayerState
+
+data class MusicControllerUiState(
+    val playerState: PlayerState? = null,
+    val currentSong: Song? = null,
+    val currentPosition: Long = 0L,
+    val totalDuration: Long = 0L,
+    val isShuffleEnabled: Boolean = false,
+    val isRepeatOneEnabled: Boolean = false
+)
