49644241e60b5bfa9c00834e1320997cd6895ee0
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-17 19:11:09 +0530

    Create Repository Module

--- app/src/main/java/com/example/composemusicplayer/di/RepositoryModule.kt
+++ app/src/main/java/com/example/composemusicplayer/di/RepositoryModule.kt
@@ -1,4 +1,30 @@
 package com.example.composemusicplayer.di
 
+import android.content.Context
+import com.example.composemusicplayer.data.remote.ApiService
+import com.example.composemusicplayer.data.repository.MusicRepository
+import com.example.composemusicplayer.data.service.MusicControllerImpl
+import com.example.composemusicplayer.domain.service.MusicController
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideRepository(apiService: ApiService): MusicRepository {
+        return MusicRepository(apiService)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMusicController(@ApplicationContext context: Context): MusicController =
+       MusicControllerImpl(context)
+
 }
