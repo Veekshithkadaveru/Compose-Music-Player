5d63ad291f09d82a4a3a14a64579eb281d61e17f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-18 20:26:19 +0530

    Add NetworkOff composable to display network disconnection UI

--- app/src/main/java/com/example/composemusicplayer/ui/components/NetworkGone.kt
+++ app/src/main/java/com/example/composemusicplayer/ui/components/NetworkGone.kt
@@ -1,2 +1,55 @@
 package com.example.composemusicplayer.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composemusicplayer.R
+
+@Composable
+fun NetworkOff(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Icon(
+            painter = painterResource(id = R.drawable.wifioff),
+            contentDescription = "Network Off",
+            tint = Color.White,
+            modifier = Modifier.size(180.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Network Disconnected",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun NetworkOffPreview() {
+    NetworkOff()
+}
