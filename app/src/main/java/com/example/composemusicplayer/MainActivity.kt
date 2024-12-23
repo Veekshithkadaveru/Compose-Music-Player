75e1fa1c5eedb6970f54d997a31c1016664474f9
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-23 22:40:57 +0530

    Add splash screen with custom exit animation and ViewModel setup in MainActivity

--- app/src/main/java/com/example/composemusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/composemusicplayer/MainActivity.kt
@@ -1,46 +1,90 @@
 package com.example.composemusicplayer
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composemusicplayer.ui.theme.ComposeMusicPlayerTheme
+import androidx.compose.ui.graphics.Color
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.composemusicplayer.data.service.MusicPlayerService
+import com.example.composemusicplayer.navigation.NavHostScreen
+import com.example.composemusicplayer.ui.view.activity.MainViewModel
+import com.example.composemusicplayer.ui.viewmodel.NetworkViewModel
+import com.example.composemusicplayer.ui.viewmodel.SharedViewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val sharedViewModel: SharedViewModels by viewModels()
+    private val splashViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ComposeMusicPlayerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        enableEdgeToEdge()
+        val splashScreen = installSplashScreen().apply {
+            setOnExitAnimationListener { viewProvider ->
+                ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    "scaleX",
+                    0.5f, 0f
+                ).apply {
+                    interpolator = OvershootInterpolator()
+                    duration = 300
+                    doOnEnd { viewProvider.remove() }
+                    start()
+                }
+                ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    "scaleY",
+                    0.5f, 0f
+                ).apply {
+                    interpolator = OvershootInterpolator()
+                    duration = 300
+                    doOnEnd { viewProvider.remove() }
+                    start()
                 }
             }
         }
+        splashScreen.setKeepOnScreenCondition {
+            splashViewModel.isLoading.value
+        }
+        setContent {
+            val networkViewModel = remember { NetworkViewModel(this) }
+            val isConnected = networkViewModel.networkStatus
+
+            val navController = rememberNavController()
+            NavHostScreen(
+                navController = navController,
+                sharedViewModel = sharedViewModel,
+                isConnected = isConnected,
+                modifier = Modifier
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sharedViewModel.destroyMediaController()
+        stopService(Intent(this, MusicPlayerService::class.java))
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SplashCompose(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeMusicPlayerTheme {
-        Greeting("Android")
-    }
 }
